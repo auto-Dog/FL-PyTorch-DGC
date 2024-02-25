96dbdb7656b99df1ca7cb9abddd7198e077d4f06
auto-Dog <2969860272@qq.com>
2024-02-25 22:07:19 +0800

    Back to no momentum and no sparse

--- src/update.py
+++ src/update.py
@@ -99,7 +99,7 @@ class LocalUpdate(object):
                 batch_loss.append(loss.item())
             epoch_loss.append(sum(batch_loss)/len(batch_loss))
         gradient_update_partial = calculate_gradient(model_dict_ori,model.state_dict())
-        gradient_update_u = merge_gradient_withmomentum(gradient_update_partial,last_update,0.5)   # 考虑动量的梯度更新 u_k,t
+        gradient_update_u = merge_gradient_withmomentum(gradient_update_partial,last_update,0.0)   # 考虑动量的梯度更新 u_k,t
         last_update_out = gradient_update_u   # 输出并存储uk,t
         gradient_update_v = merge_gradient(gradient_store,gradient_update_u)    # v_kt = v_kt-1 + u_kt
         # sparse_rates = [0.75,0.9375,0.9843,0.99]    # 热身阶段的稀疏率
@@ -108,8 +108,9 @@ class LocalUpdate(object):
             sparse_rate = sparse_rates[global_round]
         else:
             sparse_rate = sparse_rates[-1]
-        gradient_update, gradient_store = self.sparse_gradient_mask(gradient_update_v,sparse_rate=sparse_rate)    # 计算稀疏后的~G_t和被稀疏部分G_t
+        gradient_update, gradient_store = self.sparse_gradient_mask(gradient_update_v,sparse_rate=0.01)    # 计算稀疏后的~G_t和被稀疏部分G_t
         # gradient_update = gradient_update_v # debug
+        # debug: 102行动量设为0，111行稀疏率设为0.01
         del model
         del model_dict_ori  # 释放内存
         return gradient_update,gradient_store,last_update_out,sum(epoch_loss) / len(epoch_loss)
@@ -213,7 +214,7 @@ def merge_gradient(gradient1:dict,gradient2:dict)->dict:
     '''
     if gradient1.keys() != gradient2.keys():
         raise RuntimeError('Models do not has same structure')
-    gradient = {}
+    gradient = copy.deepcopy(gradient1)
     for key,_ in gradient1.items():
         gradient[key] = gradient1[key] + gradient2[key]
 
@@ -226,7 +227,7 @@ def merge_gradient_withmomentum(gradient1:dict,gradient2:dict,momentum = 0.5)->d
     '''
     if gradient1.keys() != gradient2.keys():
         raise RuntimeError('Models do not has same structure')
-    gradient = {}
+    gradient = copy.deepcopy(gradient1)
     for key,_ in gradient1.items():
         gradient[key] = gradient1[key] + gradient2[key] * momentum
 
